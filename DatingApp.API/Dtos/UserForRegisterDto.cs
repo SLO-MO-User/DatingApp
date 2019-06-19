26a41675b30e091466f7537f8520c07de8b35a27
SLO-MO-User <tuncel.halit@hotmail.com>
2019-06-19 16:18:45 +0300

    End of section 4

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -9,7 +9,7 @@ namespace DatingApp.API.Dtos
         public string Username { get; set; }
 
         [Required]
-        [StringLength(12, MinimumLength=6, ErrorMessage="You must specify the password between 6 and 12 characters.")]
+        [StringLength(8, MinimumLength=4, ErrorMessage="You must specify the password between 4 and 8 characters.")]
         public string Password { get; set; }
     }
 }
