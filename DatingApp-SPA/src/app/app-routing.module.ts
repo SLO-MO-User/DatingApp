491c1dc98a7daf5c7ed5d8583ad275a7201291b1
SLO-MO-User <tuncel.halit@hotmail.com>
2019-06-24 14:10:03 +0300

    End of section 10

--- DatingApp-SPA/src/app/app-routing.module.ts
+++ DatingApp-SPA/src/app/app-routing.module.ts
@@ -8,6 +8,9 @@ import { MemberListComponent } from './members/member-list/member-list.component
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
 import { MemberDetailResolver } from './_resolvers/member-detail.resolver';
 import { MemberListResolver } from './_resolvers/member-list.resolver';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolvers/member-edit.resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -16,8 +19,22 @@ const routes: Routes = [
     runGuardsAndResolvers: 'always',
     canActivate: [AuthGuard],
     children: [
-      { path: 'members', component: MemberListComponent, resolve: {users: MemberListResolver} },
-      { path: 'members/:id', component: MemberDetailComponent, resolve: {user: MemberDetailResolver} },
+      {
+        path: 'members',
+        component: MemberListComponent,
+        resolve: { users: MemberListResolver }
+      },
+      {
+        path: 'members/:id',
+        component: MemberDetailComponent,
+        resolve: { user: MemberDetailResolver }
+      },
+      {
+        path: 'member/edit',
+        component: MemberEditComponent,
+        resolve: { user: MemberEditResolver },
+        canDeactivate: [PreventUnsavedChanges]
+      },
       { path: 'messages', component: MessagesComponent },
       { path: 'lists', component: ListsComponent }
     ]
